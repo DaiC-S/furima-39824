b72ea02998d502adfb91b9cab46fd64115881bcf
DaiC-S <yuangudazhi@gmail.com>
2023-08-28 12:22:13 +0900

    database.yml内のdevelopment, testのDB名変更
    database.yml内のdevelopment, testのdatabase:キーのバリューであるDB名を、db:createしたときに自動生成された名前に戻した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'daichi' && password == 'dddd'
+    end
+  end
 end
