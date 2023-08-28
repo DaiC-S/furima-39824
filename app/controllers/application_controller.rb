16f12da3d5b64628b2505f0aaffa1cb4d4299aa1
DaiC-S <yuangudazhi@gmail.com>
2023-08-28 12:49:52 +0900

    Basic認証で使用する環境変数の設定
    ・BASIC_AUTH_USER
    ・BASIC_AUTH_PASSWORD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def basic_authentication
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'daichi' && password == 'dddd'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
