f752e10a453bf0d1f446e95a2d9d53aa5aecf524
DaiC-S <yuangudazhi@gmail.com>
2023-08-29 16:26:05 +0900

    新規登録の実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_29_020653) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
