6e1a5d262a93ad8a42b79cfdad28c2fb44c1c548
DaiC-S <yuangudazhi@gmail.com>
2023-08-29 19:21:56 +0900

    【修正】正規表現で長音記号を含める

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヴー-龥々]+\z/ }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
