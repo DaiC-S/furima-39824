eee74a8bd771411251dbc5c75e8bbdcf2f006606
DaiC-S <yuangudazhi@gmail.com>
2023-08-29 19:43:37 +0900

    【修正】正規表現でひらがな・カタカナ両方に長音記号を含める

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :last_name, :first_name, format: { with: /\A[ぁ-んーァ-ヴー-龥々]+\z/ }
-  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヴー－]+\z/ }
 end
