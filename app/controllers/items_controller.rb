46ccc7934c11e65ff2f0c4ed87e2d8a61a3d8a88
DaiC-S <yuangudazhi@gmail.com>
2023-08-30 15:59:32 +0900

    ActiveHashを用いて基本的に変更されないデータを表示させるための実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,25 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      rdirect_to  '/'
+    else 
+      render '/items/new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def move_to_top
     unless user_signed_in?
       redirect_to action: :index  
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   belongs_to :user
+  has_one_attached :image
+
+  validates :image,:name,:info,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:scheduled_delivery_id,:price, presence: true
+  validates :category_id,:status_id,:shipping_fee_id,:prefecture_id,:scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
 end
