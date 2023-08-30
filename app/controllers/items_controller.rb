355fddfd75c10e67448a4a2d45dfcfb958f7255f
DaiC-S <yuangudazhi@gmail.com>
2023-08-30 11:18:42 +0900

    未ログインユーザーが/items/newへ直接アクセスしたときにトップページへ戻すための実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_top, except: [:index]
+
   def index
   end
+
+  def new
+  end
+
+  private
+
+  def move_to_top
+    unless user_signed_in?
+      redirect_to action: :index  
+    end
+  end
+
 end
