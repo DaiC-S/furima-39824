93507e28ec5695aa27d1c27fa85722d635c2cc38
DaiC-S <yuangudazhi@gmail.com>
2023-09-01 20:26:39 +0900

    クライアントサイドでPAY.JPのAPIを使用するために必要なJavaScriptの読み込み

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "fee_calculation", to: "fee_calculation.js"
 pin "profit_calculation", to: "profit_calculation.js"
+pin "card", to: "card.js"
