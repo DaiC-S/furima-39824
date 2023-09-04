9d11202cb1d33f5ef46454c2b7befe1f17f26b3d
DaiC-S <yuangudazhi@gmail.com>
2023-09-04 18:29:25 +0900

    【修正】同じコードをprivateにまとめ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item only:[:index, :create]
   before_action :please_signed_in
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
 
     return unless !@item.order.nil? || current_user == @item.user
@@ -14,7 +13,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
 
     if @order_address.valid?
@@ -36,6 +34,10 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_number, :building, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id].to_i, token: params[:token]
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def buy_item
