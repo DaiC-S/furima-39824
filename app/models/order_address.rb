2038fc08251c7f3c156501066a4ce41ef4a789c7
DaiC-S <yuangudazhi@gmail.com>
2023-09-04 18:29:57 +0900

    【修正】カラムの指定を修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -15,8 +15,8 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id:, item_id:)
-    Address.create(order_id: order.id, postal_code:, prefecture_id:, city:,
-                   street_number:, building:, phone_number:)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, 
+                   street_number: street_number, building: building, phone_number: phone_number)
   end
 end
